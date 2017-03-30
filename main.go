5cc3fd3334f3ba6ce8cf44d46ef851e9808efa84
Lucas Pirola <lucaspirola@gmail.com>
2017-03-30 20:56:30 -0300

    Better code format; Setup name to builded image

--- main.go
+++ main.go
@@ -1,49 +1,48 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"log"
-	//	"net/http"
-	"github.com/gocarina/gocsv"
-	"os"
+  "github.com/gin-gonic/gin"
+  "log"
+  "github.com/gocarina/gocsv"
+  "os"
 )
 
 type Redirect struct { // Our example struct, you can use "-" to ignore a field
-	Url_Origin      string `csv:"url_origin"`
-	Url_Destination string `csv:"url_destination"`
+  Url_Origin      string `csv:"url_origin"`
+  Url_Destination string `csv:"url_destination"`
 }
 
 func main() {
-	port := os.Getenv("PORT")
+  port := os.Getenv("PORT")
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+  if port == "" {
+    log.Fatal("$PORT must be set")
+  }
 
-	router := gin.Default()
-	router.Use(gin.Logger())
-	// router.LoadHTMLGlob("templates/*.tmpl.html")
-	// router.Static("/static", "static")
+  router := gin.Default()
+  router.Use(gin.Logger())
+  // router.LoadHTMLGlob("templates/*.tmpl.html")
+  // router.Static("/static", "static")
 
-	redirectsFile, err := os.OpenFile("redirects.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
-	if err != nil {
-		panic(err)
-	}
-	defer redirectsFile.Close()
+  redirectsFile, err := os.OpenFile("redirects.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
+  if err != nil {
+    panic(err)
+  }
+  defer redirectsFile.Close()
 
-	redirects := []*Redirect{}
+  redirects := []*Redirect{}
 
-	if err := gocsv.UnmarshalFile(redirectsFile, &redirects); err != nil { // Load clients from file
-		panic(err)
-	}
+  if err := gocsv.UnmarshalFile(redirectsFile, &redirects); err != nil { // Load clients from file
+    panic(err)
+  }
 
-	router.GET("/", func(c *gin.Context) {
-		for _, redir := range redirects {
-			if c.Request.Host == redir.Url_Origin {
-				c.Redirect(301, redir.Url_Destination)
-			}
-		}
-	})
+  router.GET("/", func(c *gin.Context) {
+    for _, redir := range redirects {
+      if c.Request.Host == redir.Url_Origin {
+        c.Redirect(301, redir.Url_Destination)
+      }
+    }
+  })
 
-	router.Run(":" + port)
+  router.Run(":" + port)
 }
